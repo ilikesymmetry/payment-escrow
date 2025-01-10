28121a122e36ccf844c8c2c8df0d808ba81bb2d9
Conner Swenberg <conner.swenberg@coinbase.com>
2025-01-10 09:42:56 -0800

    Add deploy script

---
+++ script/Deploy.s.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {Script, console2} from "forge-std/Script.sol";
+import {Strings} from "openzeppelin-contracts/contracts/utils/Strings.sol";
+import {SpendPermissionManager} from "spend-permissions/SpendPermissionManager.sol";
+
+import {PaymentEscrow} from "../src/PaymentEscrow.sol";
+
+/**
+ * @notice Deploy the PaymentEscrow contract.
+ *
+ * forge script Deploy --account dev --sender $SENDER --rpc-url $BASE_SEPOLIA_RPC --broadcast -vvvv
+ * --verify --verifier-url $SEPOLIA_BASESCAN_API --etherscan-api-key $BASESCAN_API_KEY
+ */
+contract Deploy is Script {
+    function run() public {
+        // v1 https://github.com/coinbase/spend-permissions/releases/tag/v1.0.0
+        SpendPermissionManager spendPermissionManager =
+            SpendPermissionManager(0xf85210B21cC50302F477BA56686d2019dC9b67Ad);
+
+        vm.startBroadcast();
+
+        new PaymentEscrow{salt: 0}();
+
+        vm.stopBroadcast();
+    }
+}
